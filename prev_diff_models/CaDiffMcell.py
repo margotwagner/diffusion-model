d4f93d22f3a95cc9a00a7bf52747cd882dc2e9f4
Margot Wagner <mwagner92796@gmail.com>
2021-06-22 15:42:46 -0700

    updated to count CA in snare zone

--- prev_diff_models/CaDiffMcell.py
+++ prev_diff_models/CaDiffMcell.py
@@ -35,7 +35,6 @@ def time_hist(dir,iter_num, plot=True):
     ca_hist = []
     for seed in sorted(os.listdir(mcell_viz_dir)):
 
-
         # data file location
         iter_file = os.path.join(mcell_viz_dir, seed, 'Scene.ascii.{}.dat'.format(iter_num))
 
@@ -50,17 +49,23 @@ def time_hist(dir,iter_num, plot=True):
 
         snare_ca_tot = 0        # number of calcium in SNARE rxn zone
         for x,y in snare_locs:
-            print()
-            print(x,y)
+            #print()
+            #print("SNARE LOCATION:\t", x,y)
+
+            # Ugly method
             for ca_x, ca_y, ca_z in zip(loc_data['x'], loc_data['y'], loc_data['z']):
                 if ca_z <= z + rxn_rad:
-                    print(ca_x, ca_y, ca_z)
+                    if (ca_x >= x - rxn_rad) & (ca_x <= x + rxn_rad):
+                        if (ca_y >= y - rxn_rad) & (ca_y <= y + rxn_rad):
+                            print("CA LOCATION:\t", ca_x, ca_y, ca_z)
 
+            # Slick method
             snare_ca_tot += len(loc_data[(loc_data['x'] >= x - rxn_rad) & (loc_data['x'] <= x + rxn_rad) 
                     & (loc_data['y'] >= y - rxn_rad) & (loc_data['y'] <= y + rxn_rad) 
                     & (loc_data['z'] <= z + rxn_rad)])
 
-        print(snare_ca_tot)
+        #print()
+        print("TOTAL:\t\t", snare_ca_tot)
 
         for value in loc_data['r'].values:
             ca_hist.append(value)
@@ -82,14 +87,23 @@ def time_hist(dir,iter_num, plot=True):
 mcell_viz_dir = "/Users/margotwagner/ucsd/research/DiffusionModel/" \
                 "rect_zeroflux_nocalbpmca_files/mcell/output_data/viz_data_ascii"
 
+# get time points
+time_pts = [scene.split('.')[2] for scene in os.listdir(os.path.join(mcell_viz_dir, 'seed_00001'))]
+time_pts.sort()
 
+#sample points
+nums = [time_pts[i] for i in range(len(time_pts)) if i%1==0]
+print(nums)
 
 #nums = ['0050', '0100', '0500','1000']
-nums = ['0010']
+#nums = ['0010']
 #nums = ['00500','01910','04280', '10000']
 
 data = []
 for num in nums:
+    print()
+    print()
+    print("TIME POINT:\t", num)
     ca_hist = time_hist(mcell_viz_dir, num, plot=False)
     #sns.distplot(ca_hist, label=num, bins=100)
 
