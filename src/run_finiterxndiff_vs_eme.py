afa75244c196087b45f3e9ac76fb2de779910661
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2023-11-20 11:11:45 -0800

    Everything seems to be working

--- src/run_finiterxndiff_vs_eme.py
+++ src/run_finiterxndiff_vs_eme.py
@@ -34,10 +34,9 @@ def main():
     )
     fd.simulate_diffusion()
 
-    # Spectral Method (calbindin reactions)
     plotter = pm.PlotMultiRuns(
-        rw_dir="../data/eme-validation/random-walk/20231112_234059/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
-        eme_dir="../data/eme-validation/markov-eme/20231113_103340/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
+        rw_dir="../data/eme-validation/random-walk/20231120_111038/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
+        eme_dir="../data/eme-validation/markov-eme/20231120_111028/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
         n_runs=10,
         n_spatial_locs=101,
         n_time_pts=100,  # TODO: make automatic
--- src/run_finiterxndiff_vs_rw.py
+++ src/run_finiterxndiff_vs_rw.py
@@ -34,17 +34,16 @@ def main():
     )
     fd.simulate_diffusion()
 
-    # Spectral Method (calbindin reactions)
     plotter = pm.PlotMultiRuns(
-        rw_dir="../data/eme-validation/random-walk/20231112_234059/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
-        eme_dir="../data/eme-validation/markov-eme/20231113_103340/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
+        rw_dir="../data/eme-validation/random-walk/20231120_111038/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
+        eme_dir="../data/eme-validation/markov-eme/20231120_111028/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
         n_runs=10,
         n_spatial_locs=101,
         n_time_pts=100,  # TODO: make automatic
         particle_start_loc=58,
         n_particles=50,
-        plot_eme=False,  # TODO: fix LHS entry
-        plot_rw=True,
+        plot_eme=True,  # TODO: fix LHS entry
+        plot_rw=False,
     )
 
     plotter.plot_rw_eme_vs_finndiff(fd)
