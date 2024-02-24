8e21069770faf99f17724ce230531862f5b98025
Kameron Gano <103277185+kgano-ucsd@users.noreply.github.com>
2024-02-24 13:48:02 -0800

    EME data is not getting generated properly in margot branch

--- src/plot_multiruns.py
+++ src/plot_multiruns.py
@@ -5,7 +5,7 @@ PATH = "20231112_233047"
 
 plotter = pm.PlotMultiRuns(
     rw_dir="../data/eme-validation/random-walk/20231112_234059/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
-    eme_dir="../data/eme-validation/markov-eme/20240130_111708/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
+    eme_dir="../data/eme-validation/markov-eme/20240212_145332/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
     n_runs=10,
     n_spatial_locs=101,
     n_time_pts=100,  # TODO: make automatic
--- src/run_finiterxndiff_vs_eme.py
+++ src/run_finiterxndiff_vs_eme.py
@@ -36,12 +36,12 @@ def main():
 
     plotter = pm.PlotMultiRuns(
         rw_dir="../data/eme-validation/random-walk/20231120_111038/rw-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/random-walk/20230118_192600/rw-run-{}.csv",
-        eme_dir="../data/eme-validation/markov-eme/20240125_122015/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
+        eme_dir="../data/eme-validation/markov-eme/20240212_145332/eme-run-{}.csv",  # "/Users/margotwagner/diffusion-model/data/eme-validation/markov-eme/20230405_105433_2/eme-run-{}.csv",
         n_runs=10,
         n_spatial_locs=101,
         n_time_pts=100,  # TODO: make automatic
-        particle_start_loc=58,
-        n_particles=50,
+        particle_start_loc=59,
+        n_particles=58,
         plot_eme=True,  # TODO: fix LHS entry
         plot_rw=False,
     )
